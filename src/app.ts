93a23eb42f65ebd994517449770c427c2ad2e073
sameer550 <sameerbb105@gmail.com>
2023-04-02 03:19:03 +0100

    Error Handling

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,26 @@
 import "dotenv/config";
 import express, { NextFunction, Request, Response } from "express";
 import notesRouter from "./routes/note";
+import morgan from "morgan";
+import createHttpError, { isHttpError } from "http-errors";
 
 const app = express();
+app.use(morgan("dev"));
 app.use(express.json());
 app.use("/api/notes", notesRouter);
 
 app.use((req, res, next) => {
-  next(Error("Endpoint not founds please try again"));
+  next(createHttpError(404, "Endpoint not founds please try again"));
 });
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {
   console.log(error);
   let errorMessage = "An unknow error is occured";
-  if (error instanceof Error) errorMessage = error.message;
-  res.status(500).json({ error: errorMessage });
+  let statusCode = 500;
+  if (isHttpError(error)) {
+    statusCode = error.status;
+    errorMessage = error.message;
+  }
+  res.status(statusCode).json({ error: errorMessage });
 });
 export default app;
